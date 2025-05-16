0d05e8fc3ca8058a750679c6a251eedd0b496614
kellsonphilips <kellsonphilips@gmail.com>
2025-05-16 11:17:14 +0100

    add api

---
+++ app/api/auth/route.js
@@ -0,0 +1,7 @@
+export function GET(request) {
+  return new Response("Hello, Next.js!");
+}
+// export async function POST(request) {
+//   const data = await request.json();
+//   return new Response(JSON.stringify(data), { status: 201 });
+// }
