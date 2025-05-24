e88a694fd3f0b867b8d7b3e65aaa3f222f0a7783
kellsonphilips <kellsonphilips@gmail.com>
2025-05-24 10:55:17 +0100

    api

--- app/api/counter/route.js
+++ app/api/counter/route.js
@@ -1,9 +1,13 @@
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
 export async function POST(request) {
+  const client = new OpenAI();
+
   try {
     const body = await request.json();
     const { action, counterId } = body;
+    
 
     console.log(
       `${
@@ -14,6 +18,12 @@ export async function POST(request) {
     // save to database
     // Do something with the data
     // Call another API
+    const response = await client.responses.create({
+      model: "gpt-4.1",
+      input: "Write a one-sentence bedtime story about a unicorn.",
+    });
+
+    console.log(response.output_text);
 
     return NextResponse.json(
       {
